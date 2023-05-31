38806381cf76676d6dc430ffb480c423ee7536f4
MichaelWiss <michael.wiss@gmail.com>
2023-05-31 16:45:00 -0500

    createBook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        console.log('Need to add book with:', title);
+    };
+
+
     return <div>App</div>;
 }
 
