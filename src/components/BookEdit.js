92138a334aca25b95cbf0fb66c85a94c48b8d9b3
MichaelWiss <michael.wiss@gmail.com>
2023-06-13 16:44:17 -0500

    BookEdit Form

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,11 @@
 function BookEdit() {
-    return <div>BookEdit</div>;
+    return <form className="book-edit">
+        <label htmlFor="">Title</label>
+        <input className="input" />
+        <button className="button is-primary">
+            Save
+        </button>
+    </form>
 }
 
 export default BookEdit;
