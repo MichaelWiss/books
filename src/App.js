5550160953812257c8bb2c6528a1a3ede4a64b79
MichaelWiss <michael.wiss@gmail.com>
2023-06-27 16:07:20 -0500

    updating books

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -26,24 +27,29 @@ function App() {
             setBooks(updatedBooks);
         };
 
-    const createBook = (title) => {
-        axios.post()
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
 
-    //     const updatedBooks = [
-    //         ...books,
-    //         { id: Math.round(Math.random() * 9999),
-    //           title }
-    //     ];
+        console.log(response);
 
-    //     setBooks(updatedBooks);
-    // };
+        const updatedBooks = [
+            ...books,
+           response.data
+        ];
+
+        setBooks(updatedBooks);
+    };
 
 
-    return <div className="app">
+    return (
+        <div className="app">
         <h1>Reading List</h1>
         <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
         <BookCreate onCreate={createBook}/>
-    </div>;
+    </div>
+    );
 }
 
 export default App;
