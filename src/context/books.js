2ed45157b94fcde2319274e2967e81698e3aad9c
MichaelWiss <michael.wiss@gmail.com>
2023-07-06 17:18:58 -0500

    context refactor

--- src/context/books.js
+++ src/context/books.js
@@ -3,6 +3,13 @@ import { createContext, useState } from "react";
 const BooksContext = createContext();
 
 function Provider({ children }) {
+    const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
    
 
     return <BooksContext.Provider value={{}}>
