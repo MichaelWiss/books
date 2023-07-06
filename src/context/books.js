81e3f84b3032a88529e73bb654bb788cdda72e7a
MichaelWiss <michael.wiss@gmail.com>
2023-07-06 17:20:11 -0500

    using context

--- src/context/books.js
+++ src/context/books.js
@@ -10,6 +10,44 @@ function Provider({ children }) {
 
         setBooks(response.data);
     };
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
+
+        console.log(response);
+
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+               return {...book, ...response.data }; 
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookById = async (id) => {
+            await axios.delete(`http://localhost:3001/books/${id}`);
+
+            const updatedBooks = books.filter((book) => {
+                return book.id !== id;
+            });
+
+            setBooks(updatedBooks);
+        };
+
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
+
+
+        const updatedBooks = [...books, response.data];
+        setBooks(updatedBooks);
+    };
    
 
     return <BooksContext.Provider value={{}}>
