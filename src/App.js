d7536a18c2c99195b397008147a05b150a841f26
MichaelWiss <michael.wiss@gmail.com>
2023-07-06 17:20:02 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -5,49 +5,11 @@ import BookList from './components/BookList';
 
 
 function App() {
-    
-
     useEffect(() => {
         fetchBooks();
     }, []);
 
-    const editBookById = async (id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`, {
-            title: newTitle,
-        });
-
-        console.log(response);
-
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-               return {...book, ...response.data }; 
-            }
-
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    };
-
-    const deleteBookById = async (id) => {
-            await axios.delete(`http://localhost:3001/books/${id}`);
-
-            const updatedBooks = books.filter((book) => {
-                return book.id !== id;
-            });
-
-            setBooks(updatedBooks);
-        };
-
-    const createBook = async (title) => {
-        const response = await axios.post('http://localhost:3001/books', {
-            title,
-        });
-
-
-        const updatedBooks = [...books, response.data];
-        setBooks(updatedBooks);
-    };
+    
 
 
     return (
